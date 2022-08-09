d70ddabcc1f8ede04d1ead323b8edc009f3ddb61
borovskayana <borovskayana7@gmail.com>
2022-08-09 07:08:05 +0200

    J.

--- site/index.js
+++ site/index.js
@@ -2,7 +2,7 @@ window.onload = () => {
     let input = document.querySelector("#input");
     input.oninput = function() {
         let value = this.value.trim();
-        let list = document.querySelectorAll("#home-titles div");
+        let list = document.querySelectorAll(".searchdiv");
 
         if (value != "") {
 
@@ -21,3 +21,44 @@ window.onload = () => {
         console.log(this.value);
     };
 };
+
+
+$(document).ready(function(){
+ $("#home-titles").slick({
+    arrows:true,
+     dots:true,
+      slidesToShow:3,
+      initialSlide:2,
+      centerMode:true,
+      autoplay:true,
+      autoplaySpeed:2000,
+      pauseOnFocus:false,
+      pauseOnHover:false,
+      responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+        }
+        },
+        {
+        breakpoint: 991,
+        settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+        }
+        },
+        {
+        breakpoint: 500,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+        }
+        }
+        ]
+       
+  });
+}); 
