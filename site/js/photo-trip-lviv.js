42a6afc53f5230e967844e6a37af0b4b164bc394
borovskayana <borovskayana7@gmail.com>
2022-08-11 18:51:54 +0200

    Add.

---
+++ site/js/photo-trip-lviv.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+    $(".slider-min").slick({
+         arrows:true,
+         dots:true,
+         adaptiveHight:false,
+         slidesToShow:3,
+         asNavFor:".slider-big",
+         responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+            }
+            },
+            {
+            breakpoint: 991,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots:false,
+            }
+            },
+            {
+            breakpoint: 500,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots:false,
+            adaptiveHight:true,
+            }
+            }
+            ]
+      
+  
+        
+   }); 
+   $(".slider-big").slick({
+      arrows:false,
+      fade:true,
+      asNavFor:".slider-min",
+     
+   
+     
+}); 
+});
+   
